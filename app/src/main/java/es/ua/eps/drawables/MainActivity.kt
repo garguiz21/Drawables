1cae2119da4a2f5cfc676daf5e4906317caf1c6b
gilbertoarguiz <gah6@alu.ua.es>
2022-10-25 13:55:58 +0200

    Drawables

--- app/src/main/java/es/ua/eps/drawables/MainActivity.kt
+++ app/src/main/java/es/ua/eps/drawables/MainActivity.kt
@@ -1,7 +1,10 @@
 package es.ua.eps.drawables
 
+import android.graphics.drawable.StateListDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
